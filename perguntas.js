f26510c8a93da45f64f8e5cab8b0037d25b2c05f
2C230702 <00001132735877sp@al.educacao.sp.gov.br>
2024-11-24 17:36:13 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,46 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Futebol',
+    'Qual é o maior time do Brasil?',
+    'O maior time do Brasil é o Palmeiras'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Tênis',
+    'Quem é o maior tenista do mundo?',
+    'O maior tenista do mundo é o Rafael Nadal'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Esportes',
+    'Quem é o atleta mais bem pago do atualmente?',
+    'O atleta mais bem pago atualmente é o Cristiano Ronaldo'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz "saudade" em inglês?',
+    'Saudade não possui tradução no inglês, ma podemos usar o "I miss you" como "Eu sinto sua falta".'
+)
+criaCartao(
+    'Geografia',
+    'Quando foi criada a ONU?',
+    'A ONU foi criada em 24 de outubro de 1945.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual é o país de melhor qualidade de vida?',
+    'É a Suíça.'
+)
+
+criaCartao(
+    'Anatomia Humana',
+    'Quantos ossos possui o corpo humano?',
+    'Possui 206 ossos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "irmaõs" em inglês?',
+    'Caso seja "irmaõs" no sentido de dois meninos é "brothers", mas ser for um irmão e uma irmã, é "siblins".'
 )
